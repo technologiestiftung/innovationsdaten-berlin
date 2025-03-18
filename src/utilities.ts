aeccc7655f1136760e1871271e4af5f6e5524a7d
Jakob Sawal <jakob.sawal@ts.berlin>
2025-03-18 15:39:07 +0100

    feat: add utilities

---
+++ src/utilities.ts
@@ -0,0 +1,13 @@
+const hexToRgba = (hex: string, opacity: number) => {
+	// Remove # if present
+	hex = hex.replace(/^#/, "");
+
+	// Parse RGB values
+	let r = parseInt(hex.substring(0, 2), 16);
+	let g = parseInt(hex.substring(2, 4), 16);
+	let b = parseInt(hex.substring(4, 6), 16);
+
+	return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+};
+
+export { hexToRgba };
