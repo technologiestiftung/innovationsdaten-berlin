790dfe2ba1b15142dd9dab66d05f996f2debf29a
Jakob Sawal <jakob.sawal@ts.berlin>
2025-03-25 17:00:25 +0100

    feat: new App Component Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
 import "./style.scss";
 import "./index.css";
 import { useGlobalContext } from "./GlobalContext";
-import ThemeToggle from "./components/ThemeToggle";
 import Overview from "./sections/Overview";
 import Welcome from "./sections/Welcome";
 import BigFact from "./sections/BigFact";
-import BigFactComparison from "./sections/BigFactComparison";
-import Sektoren from "./sections/Sektoren";
-import TreeMap from "./sections/TreeMap";
-import AreaChart from "./sections/AreaChart";
-import BarChart from "./sections/BarChart";
-import barcharts from "./data/barcharts.json";
+import Branchen from "./sections/Branchen";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import Innovation from "./sections/Innovation";
 
 function App() {
-	const { theme } = useGlobalContext();
+	const { theme, isMobile } = useGlobalContext();
 
 	return (
 		<>
-			<main className={theme}>
-				<Welcome />
-				<Overview />
-				<BigFact factKey="berlin" />
-				<Sektoren />
-				<TreeMap />
-				<BigFact factKey="berliner_unternehmen" />
-				<AreaChart />
-				{barcharts.map((barchart) => (
-					<BarChart key={barchart.id} chart={barchart} />
-				))}
-				<BigFactComparison factKey="kmus" />
-			</main>
-			<ThemeToggle />
+			{isMobile ? (
+				<div className="w-full p-14">
+					<h2>No Mobile Version yet...</h2>
+				</div>
+			) : (
+				<>
+					<main className={theme}>
+						<Welcome />
+						<Overview />
+						<BigFact factKey="big-fact-berlin" />
+						<Branchen />
+						<BigFact factKey="berliner_unternehmen" />
+						<Innovation />
+						<div className="w-full min-h-screen" />
+					</main>
+					<Header />
+					<Menu />
+				</>
+			)}
 		</>
 	);
 }
